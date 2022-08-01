520a742fef150e3b0fde8a6e2c717812171f5e27
Andrey Elagin <andrey.elagin@nordigy.ru>
2022-08-02 01:37:24 +0400

    up conditions

--- src/main/java/interview/Main.java
+++ src/main/java/interview/Main.java
@@ -4,16 +4,26 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    // 1. Verify that your meetings is present in getAllMeetings response.
-    var expectedMeetings = List.of(new Meeting());
+    // 1. You have list of meeting ids. You need to verify that response of getAllMeetings
+    // contains Meeting objects with given ids.
+    List<String> expectedMeetingIds = List.of("1", "2", "3");
 
     // 2. You need to verify that all participants from every meeting in getAllMeetings response
     // are from the same account (let's say that it's forbidden to participate in meetings if host
     // have different account).
   }
 
+  // Returns all meetings in system
   private static List<Meeting> getAllMeetings() {
-    return List.of();
+    Participant mike = new Participant("42", "1");
+    Participant bob = new Participant("42", "2");
+    Participant peter = new Participant("77", "1");
+    return List.of(
+        new Meeting("1", "Daily", List.of(mike, bob)),
+        new Meeting("2", "Nightly", List.of(mike, bob)),
+        new Meeting("3", "Important meeting", List.of(mike, bob)),
+        new Meeting("4", "Lonely meeting", List.of(peter))
+    );
   }
 }
 
@@ -21,9 +31,26 @@ class Meeting {
   String id;
   String displayName;
   List<Participant> participants;
+
+  public Meeting() {
+  }
+
+  public Meeting(String id, String displayName, List<Participant> participants) {
+    this.id = id;
+    this.displayName = displayName;
+    this.participants = participants;
+  }
 }
 
 class Participant {
   String accountId;
   String extensionId;
+
+  public Participant() {
+  }
+
+  public Participant(String accountId, String extensionId) {
+    this.accountId = accountId;
+    this.extensionId = extensionId;
+  }
 }
